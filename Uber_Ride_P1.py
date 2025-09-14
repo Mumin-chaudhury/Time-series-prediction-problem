0387cdb5718f5b4696c9ab49168f657b583bd2dc
Mumin <141509867+Mumin-chaudhury@users.noreply.github.com>
2025-09-14 23:56:01 +0100

    Update Uber_Ride_P1.py

--- Uber_Ride_P1.py
+++ Uber_Ride_P1.py
@@ -8,9 +8,6 @@ from sklearn.metrics import mean_squared_error, r2_score
 
 # Load the dataset
 df = pd.read_csv('ncr_ride_bookings.csv')
-print(df.head())
-
-print(pd.options.display.max_rows) 
 
 # Data Preprocessing
 df.drop_duplicates()
@@ -21,3 +18,20 @@ model.fit(X_train, Y_train)
 
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
+
+print(f"Mean Squared Error (MSE): {mse:.2f}")
+print(f"R-squared (R²): {r2:.2f}")
+
+plt.figure(figsize=(8, 6))
+sns.scatterplot(x=y_test, y=y_pred, color="blue", alpha=0.6)
+plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], color='red', linestyle='--', linewidth=2)
+plt.xlabel("Actual Prices")
+plt.ylabel("Predicted Prices")
+plt.title("Actual vs Predicted Apartment Prices (Linear Regression)")
+plt.grid(True)
+plt.tight_layout()
+plt.savefig("linear_regression_actual_vs_predicted.png")
+plt.show()
+
+
+
